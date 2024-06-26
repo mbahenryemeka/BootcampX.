db77bffca51e9f179fe4f26808dd99f3ad2b1e34
Henry <mbahenryemeka@gmail.com>
2024-06-26 19:13:57 -0400

    add cohorts and student migration and seeds

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,19 @@
+DROP TABLE students IF EXISTS;
+DROP TABLE cohorts IF EXISTS;
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE
+);
