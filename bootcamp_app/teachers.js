e066da0bea8f5da8987613a0086f646aae9a0509
Henry <mbahenryemeka@gmail.com>
2024-07-13 00:39:58 -0400

    SQL injection prevention

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,27 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const cohortName = process.argv[2]; 
+const limit = process.argv[3] || 5; 
+
+const queryString = `
+SELECT teachers.id as teacher_id, teachers.name as name, cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
-ORDER BY teacher;
-`
-  )
-  .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    })
-  });
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((teacher) => {
+      console.log(
+        `${teacher.name} has an id of ${teacher.teacher_id} and teaches the ${teacher.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error('Query error', err.stack))
+  .finally(() => pool.end());
